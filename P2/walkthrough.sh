c708925436c69d21269c8eab02f24c8276eaab66
root <aleks936@hotmail.fr>
2022-04-21 19:24:14 +0200

    Modified the names of the containers

--- P2/walkthrough.sh
+++ P2/walkthrough.sh
@@ -1,7 +1,5 @@
 # install alpine images (host)
-sudo docker build -t host_aaleksov-1 -f ./host_aaleksov .
-sudo docker build -t host_aaleksov-2 -f ./host_aaleksov .
+sudo docker build -t host_aaleksov -f ./host_aaleksov .
 
 #install routeur image
-sudo docker build -t routeur_aaleksov-1 -f ./router_aaleksov .
-sudo docker build -t routeur_aaleksov-2 -f ./router_aaleksov .
+sudo docker build -t routeur_aaleksov -f ./router_aaleksov .
