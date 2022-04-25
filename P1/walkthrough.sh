b4c16117815394b1b467fca7cc760095aa1b1c26
Ye WEN <yewen@student.42.fr>
2022-04-25 18:06:18 +0200

    Update P1 routeur name

--- P1/walkthrough.sh
+++ P1/walkthrough.sh
@@ -2,4 +2,4 @@
 sudo docker build -t host_aaleksov -f ./host_aaleksov .
 
 #install routeur image
-sudo docker build -t routeur_aaleksov -f ./router_aaleksov .
+sudo docker build -t routeur_aaleksov -f ./routeur_aaleksov .
